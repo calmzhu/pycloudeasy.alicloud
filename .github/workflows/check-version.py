c17298913068b4fbea3f58edd76c2b2de24dc943
zhuqian <10286576+calmzhu@users.noreply.github.com>
2024-01-06 15:16:03 +0800

    Use a standalone relase action

--- .github/workflows/check-version.py
+++ .github/workflows/check-version.py
@@ -1,14 +1,12 @@
 import sys
 import tomllib
-
 import requests
 
 if __name__ == '__main__':
     with open("pyproject.toml", "rb") as f:
         data = tomllib.load(f)
-        current_version = data['poetry']['version']
-
-    pypi_versions = requests.get('https://pypi.org/pypi/cloudeasy/json').json()['releases']['keys']
+        current_version = data['tool']['poetry']['version']
+    pypi_versions = requests.get('https://pypi.org/pypi/cloudeasy/json').json()['releases'].keys()
     if current_version in pypi_versions:
         print(f"{current_version} is already released and cannot be reused")
         sys.exit(1)
